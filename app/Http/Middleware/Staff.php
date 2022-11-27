f70781a514b07001d41b3b352faf5c16b6dd30d2
tinnguyentg <tinnguyen121221@gmail.com>
2022-11-28 00:54:15 +0700

    [DEV] added Staff middleware

---
+++ app/Http/Middleware/Staff.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+
+class Staff
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (! $request->user()->is_staff) {
+            redirect(RouteServiceProvider::HOME);
+        }
+
+        return $next($request);
+    }
+}
