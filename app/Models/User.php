e7226643069a77a1f36b432da83aafce25ec1ca6
tinnguyentg <tinnguyen121221@gmail.com>
2022-11-28 21:10:22 +0700

    [UPDATE] install jetstream kit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,26 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Collection;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $fillable = [
         'name',
@@ -27,57 +32,30 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    /*
-    |--------------------------------------------------------------------------
-    | Relations
-    |--------------------------------------------------------------------------
-    */
-
-    public function groups()
-    {
-        return $this->belongsToMany(Group::class, 'group_user', 'user_id', 'group_id');
-    }
-
-    public function permissions(): Collection
-    {
-        $permissions = collect([]);
-        $this->groups()->each(function ($g) use (&$permissions) {
-            $permissions = $g->permissions->map(function ($perm) {
-                return $perm->name;
-            });
-        });
-
-        return $permissions;
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | Methods
-    |--------------------------------------------------------------------------
-    */
-
-    public function hasPermissions($permissions): bool
-    {
-        $user_permissions = $this->permissions();
-
-        return collect((array) $permissions)->every(function ($perm) use ($user_permissions) {
-            return $user_permissions->contains($perm);
-        });
-    }
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
